faa5ad5d16c979433813493026d2f844ad3ec160
raul-parada <raul.parada.medina@gmail.com>
2023-04-25 14:02:09 +0200

    Update xtest_lr.py

--- xtest_lr.py
+++ xtest_lr.py
@@ -132,12 +132,9 @@ def test_evaluation_auc(dataset):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = LogisticRegression(random_state=42)
     model.fit(X, y)
-    y_pred = model.predict(X_test)
-    threshold = 20
-    y_test_binary = (y_test > threshold).astype(int)
-    y_pred_binary = (y_pred > threshold).astype(int)
-    auc_score = roc_auc_score(y_test_binary, y_pred_binary)
-    #auc = roc_auc_score(y_test, pred_prob, multi_class='ovr')
+    #y_pred = model.predict(X_test)
+    pred_prob = model.predict_proba(X_test)
+    auc = roc_auc_score(y_test, pred_prob, multi_class='ovr')
     assert round(auc, 2) >= 0.92, f"AUC is {auc}, expected 0.92 or higher"
     
 def test_evaluation_mae(dataset):
