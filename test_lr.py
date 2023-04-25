9ddc147173cf18739b8cab6db9688e1643952917
raul-parada <raul.parada.medina@gmail.com>
2023-04-25 12:00:29 +0200

    Update test_lr.py

--- test_lr.py
+++ test_lr.py
@@ -54,6 +54,7 @@ def test_train_classification_model(dataset):
     # Test that the model can fit the data
     model = LogisticRegression(random_state=42)
     X, y = dataset
+    print(X.shape)
     model.fit(X, y)    
     assert model.score(X, y) > 0.7
     X_test, y_test = generate_stochastic_data()
